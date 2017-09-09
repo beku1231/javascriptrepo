ffa71e2d8f9a9c39e00c08159667db42dad1b705
Bartosz Parzychowski <parzychowskibartosz@gmail.com>
2017-09-09 14:15:11 +0200

    datatypes, arrays, objects

---
+++ js/4-datatypes.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// TYP LICZBOWY//
+
+var wyplata = 5000
+var premia = 2500
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+// STRING - ŁAŃCUCHY//
+
+var napis = "Ala ma\nkota"
+var wyplata = "5000"
+var premia = "2500"
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+console.log(napis);
+
+// TYPY LOGICZNE//
+
+var czyJestSmog;
+czyJestSmog = true;
+
+if (czyJestSmog = true){
+        console.log("Jest SMOG");
+}else {
+        console.log("Nie ma SMOG");
+    }
+
+// TYPY SPECJALNE // 
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
+
+
+
+
+
